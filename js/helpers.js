3394b3e86ef3d3c6292ada31434e00c6c126e4b6
Kofi Nyefreh <nyefrehkofi@gmail.com>
2026-01-03 23:00:50 -1200

    fix date bug

--- js/helpers.js
+++ js/helpers.js
@@ -1,7 +1,7 @@
 // Utility to format date
 function formatDate(timeStamp) {
   const date = new Date(timeStamp);
-  console.log(`${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`);
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
 }
 
 // Capitalizing spaces
